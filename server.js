958e59ed0881d784d54366eabee738b6285686d0
Elisa De Benedettis <105230322+Giolti2@users.noreply.github.com>
2022-12-12 13:01:08 +0100

    first upload
    -prototype for the presentation

---
+++ server.js
@@ -0,0 +1,30 @@
+//server setup
+
+var express = require("express")
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static("public"));
+
+console.log("server running")
+
+var serverSocket = require("socket.io")
+
+var io = serverSocket(server)
+
+io.on("connection", newConnection)
+
+function newConnection(socket) {
+    console.log(socket.id)
+
+    socket.on("morse", function (data) {
+        this.broadcast.emit("morse", data)
+        console.log(this.id + " - morse")
+    })
+
+    socket.on("bitsend", function (data) {
+        this.broadcast.emit("bitsend", data)
+        console.log(this.id + " - bitsend")
+    })
+}
